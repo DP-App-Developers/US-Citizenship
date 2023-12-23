bf2207d9eaf3ed662d21fce2b1dd1a1df36ce5d0
Dennis Hong <dehong@ebay.com>
2023-12-22 20:06:29 -0800

    use fab

--- app/src/main/java/com/example/uscitizenship/ui/FlashCardsScreen.kt
+++ app/src/main/java/com/example/uscitizenship/ui/FlashCardsScreen.kt
@@ -1,17 +1,19 @@
 package com.example.uscitizenship.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,93 +39,94 @@ fun FlashCardsScreen(
     questions: List<Question>,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier
-            .padding(16.dp),
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         var questionCount by rememberSaveable { mutableIntStateOf(1) }
         var expanded by rememberSaveable { mutableStateOf(false) }
 
-        Text(
-            text = "Question $questionCount / 100",
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-
-        ElevatedCard(
-            modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()
-                .clickable(enabled = !expanded) { expanded = true }, // card is not clickable when it's expanded
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 6.dp
-            )
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
         ) {
-            Column(
+            Text(
+                text = "Question $questionCount / 100",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            ElevatedCard(
                 modifier = Modifier
-                    .padding(12.dp)
+                    .wrapContentHeight()
                     .fillMaxWidth()
-            ) {
-                Text(
-                    text = questions[questionCount - 1].question,
-                    fontSize = 20.sp
+                    .padding(bottom = 100.dp)
+                    .clickable(enabled = !expanded) { expanded = true }, // card is not clickable when it's expanded
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
                 )
-
-                Spacer(modifier = Modifier.height(40.dp))
-
-                if (!expanded) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_expand_circle_down_24),
-                        contentDescription = "reveal answer",
-                        tint = Color.DarkGray,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = questions[questionCount - 1].question,
+                        fontSize = 20.sp
                     )
-                } else {
-                    val bullet = "\u2022"
-                    questions[questionCount - 1].answer.forEach {
-                        Text(
-                            text = "$bullet $it",
-                            fontSize = 20.sp
+
+                    Spacer(modifier = Modifier.height(40.dp))
+
+                    if (!expanded) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_expand_circle_down_24),
+                            contentDescription = "reveal answer",
+                            tint = Color.DarkGray,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
                         )
+                    } else {
+                        val bullet = "\u2022"
+                        questions[questionCount - 1].answer.forEach {
+                            Text(
+                                text = "$bullet $it",
+                                fontSize = 20.sp
+                            )
+                        }
                     }
                 }
             }
         }
 
-        Spacer(modifier = Modifier.weight(1f))
-
-        Row(
+        ExtendedFloatingActionButton(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .padding(16.dp)
+                .align(Alignment.BottomStart),
+            onClick = {
+                expanded = false
+                if (questionCount == 1) {
+                    questionCount = 100
+                } else {
+                    questionCount--
+                }
+            },
         ) {
-            Button(
-                modifier = Modifier,
-                onClick = {
-                    expanded = false
-                    if (questionCount == 1) {
-                        questionCount = 100
-                    } else {
-                        questionCount--
-                    }
+            Text(text = "Previous")
+        }
+
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd),
+            onClick = {
+                expanded = false
+                if (questionCount == 100) {
+                    questionCount = 1
+                } else {
+                    questionCount++
                 }
-            ) {
-                Text("Previous")
-            }
-            Button(
-                modifier = Modifier,
-                onClick = {
-                    expanded = false
-                    if (questionCount == 100) {
-                        questionCount = 1
-                    } else {
-                        questionCount++
-                    }
-                }
-            ) {
-                Text("Next")
-            }
+            },
+        ) {
+            Text(text = "Next")
         }
     }
 }
@@ -135,7 +138,7 @@ fun FlashCardsPreview() {
         FlashCardsScreen(
             listOf(
                 Question(
-                    question = "1. What is the supreme law of the land?",
+                    question = "1. What is the supreme law of the land? On the civics test, some answers may change because of federal or state elections, certain judicial appointments, or due to statutory changes or updates. You must answer the question with the name of the official serving at the time of your naturalization interview. On this page, you can find the answers that may have changed on the civics test to the 2008 version of the test On the civics test, some answers may change because of federal or state elections, certain judicial appointments, or due to statutory changes or updates. You must answer the question with the name of the official serving at the time of your naturalization interview. On this page, you can find the answers that may have changed on the civics test to the 2008 version of the test On the civics test, some answers may change because of federal or state elections, certain judicial appointments, or due to statutory changes or updates. You must answer the question with the name of the official serving at the time of your naturalization interview. On this page, you can find the answers that may have changed on the civics test to the 2008 version of the test",
                     answer = listOf("the Constitution"),
                 ),
                 Question(
