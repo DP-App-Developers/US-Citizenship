d13581dbbf5d3938d765029fa98a8bcabd04b8ae
Dennis Hong <dehong@ebay.com>
2023-12-23 15:06:45 -0800

    add districts

--- app/src/main/java/com/example/uscitizenship/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/uscitizenship/ui/SettingsScreen.kt
@@ -18,12 +18,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.uscitizenship.ui.theme.USCitizenshipTheme
 
 @Composable
 fun SettingsScreen() {
-
     Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            // FIXME
+            text = "Please provide your state and district information so we can generate accurate answers for your state's capital, Governor, Senators, and Representatives.",
+            fontSize = 16.sp,
+        )
+
         val states = getStates()
         var expanded by rememberSaveable { mutableStateOf(false) }
         var selectedOptionText by rememberSaveable { mutableStateOf("Select your state") }
@@ -32,7 +38,7 @@ fun SettingsScreen() {
             expanded = expanded,
             onExpandedChange = { expanded = it },
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
+//                .align(Alignment.CenterHorizontally)
         ) {
             TextField(
                 // The `menuAnchor` modifier must be passed to the text field for correctness.
@@ -40,7 +46,7 @@ fun SettingsScreen() {
                 readOnly = true,
                 value = selectedOptionText,
                 onValueChange = {},
-                label = { Text("State") },
+                label = { Text("State or District") },
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 colors = ExposedDropdownMenuDefaults.textFieldColors(),
             )
@@ -66,14 +72,17 @@ fun SettingsScreen() {
 fun getStates() = listOf(
     "Alabama",
     "Alaska",
+    "American Samoa",
     "Arizona",
     "Arkansas",
     "California",
     "Colorado",
     "Connecticut",
     "Delaware",
+    "District of Columbia",
     "Florida",
     "Georgia",
+    "Guam",
     "Hawaii",
     "Idaho",
     "Illinois",
@@ -98,6 +107,7 @@ fun getStates() = listOf(
     "New York",
     "North Carolina",
     "North Dakota",
+    "Northern Mariana Islands",
     "Ohio",
     "Oklahoma",
     "Oregon",
@@ -110,6 +120,7 @@ fun getStates() = listOf(
     "Utah",
     "Vermont",
     "Virginia",
+    "Virgin Islands",
     "Washington",
     "West Virginia",
     "Wisconsin",
