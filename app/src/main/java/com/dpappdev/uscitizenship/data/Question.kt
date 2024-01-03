4d9ee5c717de3c83871748304fd5129969e75100
Dennis Hong <dehong@ebay.com>
2024-01-02 21:06:44 -0800

    fix crash by implementing Parcelable

--- app/src/main/java/com/dpappdev/uscitizenship/data/Question.kt
+++ app/src/main/java/com/dpappdev/uscitizenship/data/Question.kt
@@ -1,7 +1,37 @@
 package com.dpappdev.uscitizenship.data
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Question(
     val questionNumber: Int,
     val question: String,
     var answer: List<String>,
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.createStringArrayList() ?: listOf()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(questionNumber)
+        parcel.writeString(question)
+        parcel.writeStringList(answer)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Question> {
+        override fun createFromParcel(parcel: Parcel): Question {
+            return Question(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Question?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
