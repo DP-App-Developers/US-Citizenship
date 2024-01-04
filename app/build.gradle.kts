d176f6c4ec346edf6a72793f4ab1b0545cf1e24f
Dennis Hong <dehong@ebay.com>
2024-01-03 22:17:58 -0800

    splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
--- app/src/main/java/com/dpappdev/uscitizenship/MainActivity.kt
+++ app/src/main/java/com/dpappdev/uscitizenship/MainActivity.kt
@@ -3,11 +3,15 @@ package com.dpappdev.uscitizenship
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dpappdev.uscitizenship.ui.theme.USCitizenshipTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         setContent {
             USCitizenshipTheme {
                 USCitizenApp()
