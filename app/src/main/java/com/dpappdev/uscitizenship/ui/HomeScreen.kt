c2320e510197b3c9e3fec00629213942f0687a4c
Dennis Hong <dehong@ebay.com>
2026-02-09 11:31:08 -0800

    polish HomeScreen UI

--- app/src/main/java/com/dpappdev/uscitizenship/ui/HomeScreen.kt
+++ app/src/main/java/com/dpappdev/uscitizenship/ui/HomeScreen.kt
@@ -3,17 +3,24 @@
 package com.dpappdev.uscitizenship.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -27,14 +34,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.dpappdev.uscitizenship.MainScreen
+import com.dpappdev.uscitizenship.R
 import com.dpappdev.uscitizenship.ui.theme.USCitizenshipTheme
 
 @Composable
@@ -61,56 +71,77 @@ fun HomeScreen(
         Column(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Column(
-                modifier = modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState())
-                    .padding(32.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+            BoxWithConstraints(
+                modifier = modifier.weight(1f)
             ) {
-                HomeScreenCard(
-                    cardText = "All Questions",
-                    onCardClick = {
-                        if (newUser) {
-                            showBottomSheet = true
-                        } else {
-                            navController.navigate(MainScreen.AllQuestions.name)
-                        }
-                    },
-                )
+                val columns = if (maxWidth < 600.dp) 1 else 2
+                
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(columns),
+                    contentPadding = PaddingValues(vertical = 32.dp, horizontal = 64.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                item {
+                    HomeScreenCard(
+                        title = "All Questions",
+                        subtitle = "128 Questions",
+                        iconId = R.drawable.outline_book_ribbon_24,
+                        onCardClick = {
+                            if (newUser) {
+                                showBottomSheet = true
+                            } else {
+                                navController.navigate(MainScreen.AllQuestions.name)
+                            }
+                        },
+                    )
+                }
 
-                HomeScreenCard(
-                    cardText = "All Flash Cards",
-                    onCardClick = {
-                        if (newUser) {
-                            showBottomSheet = true
-                        } else {
-                            navController.navigate(MainScreen.AllFlashCards.name)
-                        }
-                    },
-                )
+                item {
+                    HomeScreenCard(
+                        title = "All Flash Cards",
+                        subtitle = "128 Questions",
+                        iconId = R.drawable.outline_cards_stack_24,
+                        onCardClick = {
+                            if (newUser) {
+                                showBottomSheet = true
+                            } else {
+                                navController.navigate(MainScreen.AllFlashCards.name)
+                            }
+                        },
+                    )
+                }
 
-                HomeScreenCard(
-                    cardText = "Starred Questions",
-                    onCardClick = {
-                        if (newUser) {
-                            showBottomSheet = true
-                        } else {
-                            navController.navigate(MainScreen.StarredQuestions.name)
-                        }
-                    },
-                )
+                item {
+                    HomeScreenCard(
+                        title = "Starred Questions",
+                        subtitle = "128 Questions",
+                        iconId = R.drawable.round_star_24,
+                        onCardClick = {
+                            if (newUser) {
+                                showBottomSheet = true
+                            } else {
+                                navController.navigate(MainScreen.StarredQuestions.name)
+                            }
+                        },
+                    )
+                }
 
-                HomeScreenCard(
-                    cardText = "Starred Flash Cards",
-                    onCardClick = {
-                        if (newUser) {
-                            showBottomSheet = true
-                        } else {
-                            navController.navigate(MainScreen.StarredFlashCards.name)
-                        }
-                    },
-                )
+                item {
+                    HomeScreenCard(
+                        title = "Starred Flash Cards",
+                        subtitle = "128 Questions",
+                        iconId = R.drawable.outline_cards_star_24,
+                        onCardClick = {
+                            if (newUser) {
+                                showBottomSheet = true
+                            } else {
+                                navController.navigate(MainScreen.StarredFlashCards.name)
+                            }
+                        },
+                    )
+                }
+                }
             }
 
             Text(
@@ -159,7 +190,9 @@ fun HomeScreen(
 
 @Composable
 fun HomeScreenCard(
-    cardText: String,
+    title: String,
+    subtitle: String,
+    iconId: Int,
     onCardClick: () -> Unit,
 ) {
     OutlinedCard(
@@ -168,18 +201,47 @@ fun HomeScreenCard(
             containerColor = MaterialTheme.colorScheme.secondaryContainer,
         ),
         border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
-        modifier = Modifier
-            .padding(bottom = 16.dp)
-            .size(width = 240.dp, height = 70.dp)
+        modifier = Modifier.height(132.dp)
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
+        Column(
             modifier = Modifier.fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 4.dp)
+                    .size(48.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = iconId),
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(29.dp),
+                    contentDescription = null,
+                )
+            }
+
             Text(
-                text = cardText,
+                text = title,
                 fontWeight = FontWeight.Bold,
                 fontFamily = FontFamily.SansSerif,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+            )
+
+            Text(
+                text = subtitle,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
             )
         }
     }
