69fe87e3c5669b0594775b55fa88258138beca7a
Dennis Hong <dehong@ebay.com>
2026-09-13 18:06:12 -0700

    update names

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.dpappdev.uscitizenship"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.0.12"
+        versionCode = 24
+        versionName = "2.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
